e65850314255bf041cd0f128cac51a7b75dd38e4
moyaogangwa <137810094+moyaogangwa@users.noreply.github.com>
2023-07-10 23:57:11 +0800

    Add files via upload

---
+++ 三大结构/leap tear.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+//打印1000年到2000年之间的闰年
+int main()
+{
+	int year = 0;
+	int count = 0;
+	for (year = 1000; year <= 2000; year++)       
+	{                                               
+		if (year % 4 == 0 && year % 100 != 0)     
+		{
+			printf("%d ", year);
+			count++;
+		}
+
+		else if (year % 400 == 0)
+		{
+			printf("%d ", year);
+			count++;
+		}
+	}
+	printf("\ncount=%d", count);
+	return 0;
+}
+
+
